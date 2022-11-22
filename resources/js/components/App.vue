eae359052636a384e5ded05324107e20dfbc20ab
s-ryota0810 <akaashi0810@gmail.com>
2022-11-22 20:13:37 +0900

    initial commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>{{ msg }}</p>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const msg = ref<string>("Hello world");
+</script>
+<style lang="scss"></style>
